80bd5f61ff1996c293f5c36ac0c4e92141dc1534
Zulfiqar-Ali <52883632+zulfiqar-ali01@users.noreply.github.com>
2025-06-10 15:04:13 +0000

    Dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'SentimentTracker',
+  description: 'Created',
 }
 
 export default function RootLayout({
